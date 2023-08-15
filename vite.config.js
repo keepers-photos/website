12a2a6735f07ca1ef828278979b78fd2b7c85975
shonenada <shonenada@gmail.com>
2023-08-15 10:58:44 +0800

    chore: build privacy.html as well

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
+const { resolve } = require('path');
+
 module.exports = {
-    root: '.',
-    build: {
-        outDir: './docs'
-    }
+  root: '.',
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+      }
+    },
+    outDir: './docs'
+  }
 }
