053ef0bc7cc7a2db3fc46d94bb9536d406abffb5
Alex Dong <alex@keepers.photos>
2024-12-13 15:33:36 +1300

    add newborn.html

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ module.exports = {
     rollupOptions: {
       input: {
         index: resolve(__dirname, "assets/index.html"),
+        newborn: resolve(__dirname, "assets/newborn.html"),
         privacy: resolve(__dirname, "assets/privacy.html"),
         news: resolve(__dirname, "assets/news.html"),
       },
